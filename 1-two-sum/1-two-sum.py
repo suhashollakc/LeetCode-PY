17239e32b9ce94c52da3c61d6e80d9dc580acf63
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-01-22 20:16:40 -0600

    Time: 71 ms (63.89%), Space: 15.4 MB (19.14%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+#       Two-Pass Hash Table 
+#         hashMap = {} #This holds the value and index
+#         for i in range(len(nums)): #for each element
+#             hashMap[nums[i]] = i #Add the key:value pair to hashmap
+#         for i in range(len(nums)):
+#             complement = target - nums[i]; #Calculating complement by subtracting target with current element
+#             if complement in hashMap and hashMap[complement] != i:
+#                 return [hashMap[complement],i]
+            
+#         One-Pass Hash Table
         hashMap = {} #This holds the value and index
         for i in range(len(nums)): #for each element
-            hashMap[nums[i]] = i #Add the key:value pair to hashmap
-        for i in range(len(nums)):
-            complement = target - nums[i]; #Calculating complement by subtracting target with current element
-            if complement in hashMap and hashMap[complement] != i:
-                return [hashMap[complement],i]
-            
+            complement = target - nums[i]
+            if complement in hashMap:
+                return [i,hashMap[complement]]
+            hashMap[nums[i]] = i
