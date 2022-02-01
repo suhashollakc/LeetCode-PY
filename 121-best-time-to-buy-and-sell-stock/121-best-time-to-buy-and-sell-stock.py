439d478a1bdc979c99abc7ec9503af1b6cdeb2c6
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-02-01 02:50:07 -0600

    Time: 1795 ms (15.78%), Space: 25.1 MB (52.84%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+#         buy = float('-inf') #max cash balance after buying  stock
+#         sell = 0 #maximum cash balance after buying and selling a stock
+        
+#         for price in prices:
+#             buy = max(-price,buy) #max of buying earlier or now
+#             sell = max(price + buy,sell) #max of selling eaarlier or now
+         
+#         return sell
+            
+        #Sliding Window Problem (Two Pointers)
+        l,r = 0,1 #left -> Buying, right -> Selling
+        maxP = 0
+        
+        while r < len(prices):
+            #profitable ?
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP,profit)
+            else:
+                l = r
+            r += 1
+        return maxP
