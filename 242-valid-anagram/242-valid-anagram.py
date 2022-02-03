b2e45d4bf0e266dd8ca33e44b46a4dec0def2c71
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-02-02 22:20:44 -0600

    Time: 81 ms (28.02%), Space: 14.5 MB (80.74%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        letterDictionary = {}
+        
+        for char in s:
+            if char not in letterDictionary:
+                letterDictionary[char] = 1
+            else:
+                letterDictionary[char] += 1
+                
+        for char in t:
+            if char not in letterDictionary:
+                return False
+            else:
+                if letterDictionary[char] == 1:
+                    del letterDictionary[char]
+                    
+                else:
+                    letterDictionary[char] -= 1
+        
+        if letterDictionary != {}:
             return False
-        return sorted(s) == sorted(t)
+        return True
+        
