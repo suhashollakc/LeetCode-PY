fd746a37df2aee800d2fae7efaec7070ed717e48
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-02-05 19:35:03 -0600

    Time: 108 ms (87.63%), Space: 14.5 MB (82.44%) - LeetHub

---
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        letters_length = len(letters)
+        low = 0
+        high = letters_length - 1
+        
+		# target is outside the bounds of letters
+		# because the array is circular, this means that the next greatest value 
+		# must be the first value in the array. If the value is within the bounds
+		# of the array we can perform our regular modified binary search
+        if target < letters[low] or target >= letters[high]:
+            return letters[low]
+
+        while low <= high:
+            middle =  (low + high) // 2
+            candidate = letters[middle]
+            
+            if target < candidate: 
+                high = middle - 1
+            
+			# becuase we're looking for the smallest value thats greater then the target
+			# we can condense the typical case where target == letters[middle] into the 
+			# the target > letters[middle] case
+            if target >= candidate :
+                low = middle + 1
+        
+        return letters[low]
