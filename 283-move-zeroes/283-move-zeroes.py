1d8cae7b186e2a9aba50d0f6bb6c7ce8fb48d3b2
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-02-25 02:20:21 -0600

    Time: 191 ms (71.29%), Space: 15.6 MB (15.19%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,12 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = None
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                if k is not None:
-                    nums[i],nums[k] = nums[k],nums[i]
-                    k += 1
-            else:
-                if k is None:
-                    k = i
+        
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast]!= 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+        
+        
+        
+        
+        
+        
+        # **********************
+        # k = None
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         if k is not None:
+        #             nums[i],nums[k] = nums[k],nums[i]
+        #             k += 1
+        #     else:
+        #         if k is None:
+        #             k = i
