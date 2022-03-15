fd1ecfa49f12d893f2669b77dc6c133a55a58d54
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-03-15 15:41:38 -0500

    Time: 472 ms (83.96%), Space: 26.1 MB (6.05%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -5,10 +5,15 @@ class Solution:
         #Make a hashset , with key -> num in nums and value -> Count(num).
         #If count(num) >=2 return true else False
         
-        nums = Counter(nums)
-        for key,val in nums.items():
-            if val >= 2:
+#         nums = Counter(nums)
+#         for key,val in nums.items():
+#             if val >= 2:
+#                 return True
+#         return False
+        
+        hashSet= set()
+        for num in nums:
+            if num in hashSet:
                 return True
+            hashSet.add(num)
         return False
-        
-        
