7bbb6dbf7ac5bdf41a53478b0e2238b6c6d30989
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-03-19 20:46:06 -0500

    Time: 76 ms (76.89%), Space: 15.2 MB (53.48%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,19 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # I have two solutions approach for this. One,
+        #Making an hash table and storing the elements with indexes and checking if the 
+        #complement is in the hashmap, if it is, we found the numbers, complement and hashmap key
         
-#       Two-Pass Hash Table 
-#         hashMap = {} #This holds the value and index
-#         for i in range(len(nums)): #for each element
-#             hashMap[nums[i]] = i #Add the key:value pair to hashmap
-#         for i in range(len(nums)):
-#             complement = target - nums[i]; #Calculating complement by subtracting target with current element
-#             if complement in hashMap and hashMap[complement] != i:
-#                 return [hashMap[complement],i]
+        #Another one is assuming, the array is not sorted or any criteria regarding it,
+        #I will sort the array, then check the sum of two elements using no extra space
+        #and if sum matches the targetSum, we found the answer.
+        
+        #Approach 1. using hashmap
+        
+        hashMap = {}
+        
+        for value in range(len(nums)):
+            hashMap[nums[value]] = value
+        
+        for i in range(len(nums)):
             
-#         One-Pass Hash Table
-        hashMap = {} #This holds the value and index
-        for i in range(len(nums)): #for each element
             complement = target - nums[i]
-            if complement in hashMap:
+            
+            if complement in hashMap and hashMap[complement] != i:
                 return [i,hashMap[complement]]
-            hashMap[nums[i]] = i
