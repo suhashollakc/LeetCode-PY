8fee40c06d17192b8fbdeffee0376d2764d96200
SUHAS HOLLA K C <51073384+suhashollakc@users.noreply.github.com>
2022-03-20 18:51:33 -0500

    Time: 172 ms (21.43%), Space: 14.2 MB (37.32%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,4 +1,35 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        mergedArray = sorted(nums1 + nums2)
-        return median(mergedArray)
+        A,B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+        
+        #I want to apply BS on Array A
+        if len(B) < len(A):
+            A,B=  B,A
+        #log(min(n,m))
+        l, r = 0, len(A) - 1
+        
+        while True:
+            i = (l+r) // 2 #For A
+            j = half - i - 2  #B As array start from 0 we do extra -1 for both
+            
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i+1] if i+1 < len(A) else float("infinity")
+            
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j+1] if j+1 < len(B) else float("infinity")
+            
+            if Aleft <= Bright and Bleft <= Aright:
+                #odd no of elemeemnts
+                if total % 2:
+                    return min(Aright,Bright)
+                #Even no of elements
+                
+                return (max(Aleft,Bleft) + min(Aright,Bright)) / 2
+            elif Aleft > Bright:
+                r = i -1
+            else:
+                l = i + 1
+                
+            
